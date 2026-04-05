7d8b11283281edf72d68ec1b10b3f8f8583c5175
Chieh-An(Andy) Chang <90326818+ChiehAnChang@users.noreply.github.com>
2026-04-04 21:37:21 -0400

    Add: house-robber-iii - submission-0

---
+++ Data Structures & Algorithms/house-robber-iii/submission-0.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def rob(self, root: Optional[TreeNode]) -> int:
+        def dfs(root):
+            if not root:
+                return [0, 0]   # [without_root, with_root]
+
+            left_res = dfs(root.left)
+            right_res = dfs(root.right)
+
+            # 不偷目前這個 root
+            without_root = max(left_res) + max(right_res)
+
+            # 偷目前這個 root
+            with_root = root.val + left_res[0] + right_res[0]
+
+            return [without_root, with_root]
+
+        return max(dfs(root))
